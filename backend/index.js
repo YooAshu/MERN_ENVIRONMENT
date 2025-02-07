693542b9d1ca380e8ac84e8127a79f32b79a2e77
Asghar Hussain <asghus117@gmail.com>
2025-02-07 19:13:13 +0530

    using imort for dotenv instead of require

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config({path : './.env'});
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" }); // ✅ Load environment variables
 import express from "express"
 
 const app = express()
