835c3eb7564516437cb91540df4271e631ad6aec
Asghar Hussain <asghus117@gmail.com>
2025-02-07 19:10:19 +0530

    added dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,13 @@
+require('dotenv').config({path : './.env'});
 import express from "express"
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000; // Use environment variable or fallback to 3000
 
 app.get('/api/hello',(req,res)=>{
     res.send('Hello World')
 })
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
